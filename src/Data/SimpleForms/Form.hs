fca9bd38e458d1f3e217485edb7c8209621a3258
uwap <me@uwap.name>
2017-04-04 12:58:02 +0200

    Lensify Things

--- src/Data/SimpleForms/Form.hs
+++ src/Data/SimpleForms/Form.hs
@@ -1,19 +1,42 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ViewPatterns #-}
 module Data.SimpleForms.Form where
 
 import Protolude
 import Text.Digestive
+import Control.Lens hiding ((&))
 
 data Validation = NotEmpty
                 | Equals Text
+              deriving (Show, Eq)
 
 data FormType = InputText
               | InputTextArea
               | Radio [Text]
               | CheckBox [Text]
+            deriving (Show, Eq)
 
 data FormField = FormField
-               { formFieldType        :: FormType
+               { formFieldFormType    :: FormType
                , formFieldDescription :: Text
                , formFieldValidations :: [Validation]
-               }
--- makeFields'' FormField
+               } deriving (Show, Eq)
+makeFields ''FormField
+
+type SimpleForm = [FormField]
+
+type FormResult = [(FormField, Text)]
+
+validateResult :: FormResult -> Bool
+validateResult [] = True
+validateResult ((f,x):xs) = validateField (f ^. validations) && validateResult xs
+  where
+    validateField :: [Validation] -> Bool
+    validateField []     = True
+    validateField (v:vs) = validateField vs &&
+      case v of
+        NotEmpty -> True -- not $ null x
+        Equals y -> x == y
